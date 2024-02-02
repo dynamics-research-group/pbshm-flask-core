b5aa7a39042be3ffa74af9667eead7dc3123ab50
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2024-02-02 17:22:13 +0000

    Create diagnostic endpoint
    Include a diagnostic endpoint to display a status of how many populations
    and stuctures there are within the default collection.

--- pbshm/layout/layout.py
+++ pbshm/layout/layout.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, g, render_template
+from flask import Blueprint, g, render_template, jsonify
 from pbshm.authentication.authentication import authenticate_request
+from pbshm.db import default_collection
 
 #Create the layout Blueprint
 bp = Blueprint(
@@ -15,3 +16,26 @@ def home():
     if g.user is None: raise Exception("No user data in global object")
     else:
         return render_template("home.html", name=g.user["firstName"])
+    
+@bp.route("/diagnostics")
+@authenticate_request("layout-diagnostics")
+def diagnostics():
+    populations = {}
+    for document in default_collection().aggregate([
+        {"$project":{
+            "_id":1,
+            "name":1,
+            "population":1
+        }},
+        {"$group":{
+            "_id":"$population",
+            "structures":{"$addToSet":"$name"}
+        }},
+        {"$project":{
+            "_id":0,
+            "population":"$_id",
+            "structures":1
+        }}
+    ]):
+        populations[document["population"]] = document["structures"]
+    return jsonify({"status":f"Total populations found {len(populations)}, with a total of {sum([len(populations[population]) for population in populations])} unique structures", "details":populations})
