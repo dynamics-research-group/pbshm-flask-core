b47e5260e6104a15323779a603391b0c2fc9cca7
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2024-02-02 15:55:15 +0000

    Include an endpoint for the home template

--- pbshm/layout/layout.py
+++ pbshm/layout/layout.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, g, render_template
+from pbshm.authentication.authentication import authenticate_request
 
 #Create the layout Blueprint
 bp = Blueprint(
@@ -7,3 +8,10 @@ bp = Blueprint(
     static_folder = "static",
     template_folder = "templates"
 )
+
+@bp.route("/home")
+@authenticate_request("layout-home")
+def home():
+    if g.user is None: raise Exception("No user data in global object")
+    else:
+        return render_template("home.html", name=g.user["firstName"])
