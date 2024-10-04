d08e15364c22edb128683c1d5e13bfd2bf1a7552
TristanGowdridge <tristan.gowdridge@gmail.com>
2024-10-04 10:04:06 +0100

    Adding support to check sha3_512 hashes, and autoupdate to pbkdf2

--- pbshm/authentication/authentication.py
+++ pbshm/authentication/authentication.py
@@ -1,14 +1,97 @@
 from functools import wraps
+import hashlib
+import hmac
 
 from bson import ObjectId
 from flask import Blueprint, g, render_template, request, session, redirect, url_for, current_app
 from werkzeug.exceptions import Unauthorized
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
 from pbshm.db import user_collection
 
 #Create the Authentication Blueprint
 bp = Blueprint("authentication", __name__, template_folder="templates")
+
+DEFAULT_PBKDF2_ITERATIONS = 600000
+
+
+def _drg_hash_internal(method, salt, password):
+    """
+    A modified version of Werkzeug's '_hash_internal', extended to support 
+    SHA3_512 for legacy purposes. Taken from Werkzeug source code (version < 2.4).
+    Deprecated hash algorithms return "DEPRECATED" to signal the need for a
+    hash update, instead of the usual method and parameters.
+    """
+    method, *args = method.split(":")
+    salt_bytes = salt.encode()
+    password_bytes = password.encode()
+
+    if method == "scrypt":
+        if not args:
+            n = 2**15
+            r = 8
+            p = 1
+        else:
+            try:
+                n, r, p = map(int, args)
+            except ValueError:
+                raise ValueError("'scrypt' takes 3 arguments.") from None
+
+        maxmem = 132 * n * r * p  # ideally 128, but some extra seems needed
+        return (
+            hashlib.scrypt(
+                password_bytes, salt=salt_bytes, n=n, r=r, p=p, maxmem=maxmem
+            ).hex(),
+            f"scrypt:{n}:{r}:{p}",
+        )
+    
+    elif method == "pbkdf2":
+        len_args = len(args)
+
+        if len_args == 0:
+            hash_name = "sha256"
+            iterations = DEFAULT_PBKDF2_ITERATIONS
+        elif len_args == 1:
+            hash_name = args[0]
+            iterations = DEFAULT_PBKDF2_ITERATIONS
+        elif len_args == 2:
+            hash_name = args[0]
+            iterations = int(args[1])
+        else:
+            raise ValueError("'pbkdf2' takes 2 arguments.")
+
+        return (
+            hashlib.pbkdf2_hmac(
+                hash_name, password_bytes, salt_bytes, iterations
+            ).hex(),
+            f"pbkdf2:{hash_name}:{iterations}",
+        )
+    
+    else:
+        return (
+            hmac.new(salt_bytes, password_bytes, method).hexdigest(),
+            "DEPRECATED"
+        )
+
+
+def drg_check_password_hash(pwhash, password):
+    """
+    Verifies if the password matches the given hash (pwhash). 
+    Returns two booleans: the first indicates if the passwords match, and the 
+    second indicates if the hash requires updating.
+    """
+    method, salt, hashval = pwhash.split('$', 2)
+    hashed_pw, method = _drg_hash_internal(method, salt, password)
+    
+    passwords_match = hmac.compare_digest(hashed_pw, hashval)
+    # Even if the hash needs updating, the hash is not updated because the
+    # passwords do not match! This is an absolute fail safe as the update
+    # actually happens inside a conditional only if the passwords match, but
+    # better to be safe than sorry!
+    needs_updating = (method == "DEPRECATED") if passwords_match else False
+
+    return passwords_match, needs_updating
+
 
 #Login View
 @bp.route("/login", methods=("GET", "POST"))
@@ -27,10 +110,19 @@ def login():
                 { "emailAddress": email_address },
                 { "_id": 1, "password": 1, "enabled": 1}
             )
-            if user is None: error = "Unable to locate these credentials."
-            elif not user["enabled"]: error = "This account has been disabled."
-            elif not check_password_hash(user["password"], password): error = "This email address and password do not match any credentials."
+            passwords_match, needs_updating = drg_check_password_hash(user["password"], password)
+            if user is None:
+                error = "Unable to locate these credentials."
+            elif not user["enabled"]:
+                error = "This account has been disabled."
+            elif not passwords_match:
+                error = "This email address and password do not match any credentials."
             else:
+                if needs_updating:
+                    user_collection().update_one(
+                        {"_id": user["_id"]},
+                        {"$set": {"password": generate_password_hash(password, "pbkdf2", 128)}}
+                    )
                 session.clear()
                 session["user_id"] = str(user["_id"])
                 return redirect("/")
